5b4a02a3828cf9004b863b8f97386390523197ab
Vanshita Rohela <vanshitarohela@gmail.com>
2024-04-27 00:08:45 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { abortOnError: false }); // throws error instead of 1
   await app.listen(3000);
 }
 bootstrap();
